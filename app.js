c8beed9ec5cbbd0725aa1d46d96c3d83959d1f72
JR-Camacho <josecamachoc0303@gmailcom>
2022-10-23 16:03:40 -0400

    Change

--- app.js
+++ app.js
@@ -80,18 +80,49 @@ app.delete('/delete-employee/:id', (req, res) => {
     })
 })
 
-//mysql connection
-const connection = mysql.createConnection({
+// //mysql connection
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME
+// });
+
+// //connect mysql
+// connection.connect(err => {
+//     if(err) throw('Connection error', err)
+//     else console.log('Connection success!');
+// })
+
+const db_config = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
-});
+}
 
-//connect mysql
-connection.connect(err => {
-    if(err) throw('Connection error', err)
-    else console.log('Connection success!');
-})
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    } else console.log('Connection success!'); // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 app.listen(PORT, () => console.log(`Server runing on port ${PORT}`));
